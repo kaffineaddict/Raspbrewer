3e8e91d6e3bc2ec3f7b2d74869aee3a76ae7d0c8
Blake Sutton <sutton.blake@gmail.com>
2015-03-17 21:44:23 -0600

    Update RelayPi.h

--- RelayPi.h
+++ RelayPi.h
@@ -1,9 +1,16 @@
 /* 
  * File:   RelayPi.h
- * Author: KaffineAddict
+ * Project: Raspbrewer
+ * Author: Blake Sutton
+ * Email: blasutto@uat.edu
  *
  * Created on March 17, 2015, 7:41 PM
+ * Last Updated: 3/17/2015
  */
+
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include <wiringPi.h>
 #include <stdio.h>
