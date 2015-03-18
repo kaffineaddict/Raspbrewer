a0b1facc7c235f9de957c615f513b8480c658259
Blake Sutton <sutton.blake@gmail.com>
2015-03-17 18:18:53 -0600

    added the license header to the source files

--- main.c
+++ main.c
@@ -8,6 +8,10 @@
  * Last Updated: 3/17/2015
  */
 
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "relayPi.h"
