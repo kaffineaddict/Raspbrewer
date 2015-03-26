ed3bcfb0e8449780a4e46d4ec7451d575d86789b
Blake Sutton <sutton.blake@gmail.com>
2015-03-26 07:29:55 -0600

    Very Rough outline of the multithreading using pthread

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@
 #include <iostream>
 #include <string.h>
 #include <ncurses.h>
+#include <pthread.h>
 #include "RelayPi.h"
 #include "TempPi.h"
 
@@ -27,6 +28,10 @@ int relay_4 = 3; // pin 15
 std::string sensor1 = "28-00000655b53a";
 std::string beer_name = "Cream Ale";
 
+pthread_t t1, t1_id, t2, t2_id;
+
+void *testThread(void *);
+void *testThread2(void *);
 void buildScreen(int, int);
 /*
  * 
@@ -38,13 +43,18 @@ int main(int argc, char** argv) {
     int count = 0;
     float main_temp = 0;
     float next_temp = 0;
-    int row,col;				/* to store the number of rows and *
+    
+    int row,col;                                 /* to store the number of rows and *
                                                 /* the number of colums of the screen */
     initscr();                                  /* start the curses mode */
     getmaxyx(stdscr,row,col);                   /* get the number of rows and columns */
     
     temp.registerSensor("main_brew", "28-00000655b53a");
     temp.registerSensor("next_brew", "28-00000657dc06");
+    
+    pthread_create( &t2, NULL, testThread2, NULL);
+    
+    pthread_create( &t1, NULL, testThread, NULL);
     
     buildScreen(row, col);
     while(running)
@@ -76,4 +86,47 @@ void buildScreen(int rows, int cols)
     std::string bar = std::string(cols, '=');
     mvprintw(LINES - 2, 0, "Brewing: %s", beer_name.c_str());
     mvprintw(LINES - 3, 0, "%s", bar.c_str());
+}
+
+void *testThread(void *ptr)
+{
+    //t1 =  pthread_self();
+    while(true)
+    {
+        char c = getch();
+        mvprintw(1, 0, "You Pressed: %c", c);
+    }
+    pthread_exit(NULL);
+}
+
+void *testThread2(void *ptr)
+{
+    RelayPi relay;
+    TempPi temp;
+    bool running = true;
+    int count = 0;
+    float main_temp = 0;
+    float next_temp = 0;
+    
+    int row,col;                                 /* to store the number of rows and *
+                                                /* the number of colums of the screen */
+    initscr();                                  /* start the curses mode */
+    getmaxyx(stdscr,row,col);                   /* get the number of rows and columns */
+    
+    temp.registerSensor("main_brew", "28-00000655b53a");
+    temp.registerSensor("next_brew", "28-00000657dc06");
+    
+    buildScreen(row, col);
+    while(running)
+    {   
+        count++;
+        main_temp = temp.readTemp("main_brew");
+        next_temp = temp.readTemp("next_brew");
+        mvprintw(LINES - 1, 0, "Frame: %d", count);     
+        mvprintw(LINES - 2, col - 18, "Main Temp: %4.2f", main_temp);
+        mvprintw(LINES - 1, col - 18, "Next Temp: %4.2f", next_temp);
+        refresh();
+        delay(250);
+    }
+    pthread_exit(NULL);
 }
